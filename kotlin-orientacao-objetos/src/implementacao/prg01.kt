7f4b9fedb34f39572f0ed0262e9bff6dd17504dd
Rogerio Vieira da Mota <rogerio.vieira.mota@gmail.com>
2022-11-04 18:02:49 -0300

    Implementacao deposita

---
+++ kotlin-orientacao-objetos/src/implementacao/prg01.kt
@@ -0,0 +1,35 @@
+package implementacao
+
+fun main() {
+    println("Bem vindo ao Bytebank")
+
+    val contaAlex = Conta()
+    val contaFran = Conta()
+
+    contaAlex.titular="Alex"
+    contaAlex.numero=1000
+    contaAlex.saldo=100.01
+
+    //val valor = 0.0
+    //contaAlex.deposita02(50.0)
+    contaAlex.deposita02(50.1)
+    //contaAlex.deposita02(50.0)
+
+}
+
+class Conta {
+    var titular=""
+    var numero=0
+    var saldo=0.0
+
+    //função membro
+    //função de métodos
+    //MÉTODO ou o método propriamente dito
+
+    fun deposita02(valor: Double){
+        this.saldo += valor
+        println(saldo)
+    }
+
+}
+
